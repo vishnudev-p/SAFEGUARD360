6d6047fd990bf335884d3b4d28c927773e0a8756
Vishnu Dev P <141658861+vishnudev-p@users.noreply.github.com>
2025-05-20 19:37:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ except ImportError:
 CONFIG = {
     "upload_folder": os.environ.get("UPLOAD_FOLDER", "Uploads"),
     "user_file": os.environ.get("USER_FILE", "users.json"),
-    "violence_model_path": os.environ.get("VIOLENCE_MODEL_PATH", "/app/models/my_model1.pt"),
-    "scream_model_path": os.environ.get("SCREAM_MODEL_PATH", "/app/models/Resnet34_Model_2023-12-05--08-37-20.pt"),
+    "violence_model_path": os.environ.get("VIOLENCE_MODEL_PATH", "/opt/render/project/src/models/my_model1.pt"),
+    "scream_model_path": os.environ.get("SCREAM_MODEL_PATH", "/opt/render/project/src/models/Resnet34_Model_2023-12-05--08-37-20.pt"),
     "email": {
         "sender": os.environ.get("EMAIL_SENDER"),
         "receiver": os.environ.get("EMAIL_RECEIVER"),
